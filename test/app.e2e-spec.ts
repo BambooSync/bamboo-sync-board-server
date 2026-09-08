ee37abe9f5ce6906b4c204abe8b345c3f70cd841
TrNhDuong <nhatduong0411@gmail.com>
2026-09-08 08:58:16 +0700

    update

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -7,7 +7,7 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -44,7 +44,7 @@ describe('AppController (e2e)', () => {
       .expect(429);
   });
 
-  afterEach(async () => {
+  afterAll(async () => {
     await app.close();
   });
 });
